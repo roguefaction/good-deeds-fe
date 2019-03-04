e761702b182dc49cbd2f3eff0a7a2cee2f0c9a28
Rolanda Raudyte <rolanda763@gmail.com>
2019-03-04 19:26:51 +0200

    Uptaded component good-deeds.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { GoodDeedsComponent } from './good-deeds/good-deeds.component';
 import { SuccessStoriesComponent } from './success-stories/success-stories.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { JobListComponent } from './job-list/job-list.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     GoodDeedsComponent,
     SuccessStoriesComponent,
     GalleryComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    RegisterFormComponent,
+    JobListComponent
   ],
   imports: [
     BrowserModule,
