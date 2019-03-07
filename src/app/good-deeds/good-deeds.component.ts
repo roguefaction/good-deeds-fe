f9ac0d0786885aca4219693389b74ee6013e7cbc
Vytautas Kondratas <vytas.k@live.com>
2019-03-07 13:54:43 +0200

    moved models into app folder and added a couple of title images

--- src/app/good-deeds/good-deeds.component.ts
+++ src/app/good-deeds/good-deeds.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {JobService} from '../services/job.service';
-import {Job} from '../../models/job';
+import {Job} from '../models/job';
 
 @Component({
   selector: 'app-good-deeds',
