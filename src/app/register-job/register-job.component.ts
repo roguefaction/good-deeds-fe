438845d030706be88171b402c83e99b9bedfd4e7
Vytautas Kondratas <vytas.k@live.com>
2019-03-08 10:15:12 +0200

    register deed submit now displays all invalid fields small style changes

--- src/app/register-job/register-job.component.ts
+++ src/app/register-job/register-job.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Job} from '../models/job';
-import {HttpResponse} from "@angular/common/http";
-import {JobService} from "../services/job.service";
-import {routerNgProbeToken} from "@angular/router/src/router_module";
+import {HttpResponse} from '@angular/common/http';
+import {JobService} from '../services/job.service';
+import {routerNgProbeToken} from '@angular/router/src/router_module';
 import {Router} from '@angular/router';
 
 @Component({
@@ -15,7 +15,8 @@ export class RegisterJobComponent implements OnInit {
 
   registerJobForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private jobService: JobService, private router: Router) { }
+  constructor(private formBuilder: FormBuilder, private jobService: JobService, private router: Router) {
+  }
 
   ngOnInit() {
     this.createForm();
@@ -32,7 +33,7 @@ export class RegisterJobComponent implements OnInit {
       description: ['', [Validators.maxLength(500)]],
       // hastags pasidometi del validacijos
       tags: ['', [Validators.maxLength(500), Validators.pattern('(#[a-zA-Z]+,?)+[^,]$')]]
-    //  /#(\w*[0-9a-zA-Z]+\w*[0-9a-zA-Z])/g
+      //  /#(\w*[0-9a-zA-Z]+\w*[0-9a-zA-Z])/g
     });
   }
 
@@ -40,17 +41,27 @@ export class RegisterJobComponent implements OnInit {
 
     if (this.registerJobForm.invalid) {
       console.log(this.registerJobForm);
-      alert('Please fix the form!');
+      this.markFormGroupTouched(this.registerJobForm);
       return;
     }
 
     if (this.registerJobForm.valid) {
-       this.router.navigate(['good-deeds']);
-       window.location.reload();
+      this.router.navigate(['good-deeds']);
+      window.location.reload();
 
     }
     this.addJob(this.registerJobForm.value);
 
+  }
+
+  private markFormGroupTouched(formGroup: FormGroup) {
+    (<any> Object).values(formGroup.controls).forEach(control => {
+      control.markAsTouched();
+
+      if (control.controls) {
+        this.markFormGroupTouched(control);
+      }
+    });
   }
 
   addJob(job: Job) {
@@ -68,7 +79,6 @@ export class RegisterJobComponent implements OnInit {
 
 
   }
-
 
 
 }
