d49818371920373ac26690d987a6e7d6c9774144
Vytautas Kondratas <vytas.k@live.com>
2019-03-08 13:49:09 +0200

    fixed register not adding to backend problem

--- src/app/register-job/register-job.component.ts
+++ src/app/register-job/register-job.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Job} from '../models/job';
-import {HttpResponse} from "@angular/common/http";
-import {JobService} from "../services/job.service";
-import {routerNgProbeToken} from "@angular/router/src/router_module";
+import {HttpResponse} from '@angular/common/http';
+import {JobService} from '../services/job.service';
 import {Router} from '@angular/router';
 
 @Component({
@@ -15,7 +14,8 @@ export class RegisterJobComponent implements OnInit {
 
   registerJobForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private jobService: JobService, private router: Router) { }
+  constructor(private formBuilder: FormBuilder, private jobService: JobService, private router: Router) {
+  }
 
   ngOnInit() {
     this.createForm();
@@ -26,13 +26,13 @@ export class RegisterJobComponent implements OnInit {
       title: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]],
       organization: ['', [Validators.minLength(5), Validators.maxLength(50)]],
       city: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50), Validators.pattern('^[A-Za-z ]+$')]],
-      emailAddress: ['', [Validators.required, Validators.email]],
+      email: ['', [Validators.required, Validators.email]],
       contactPerson: ['', [Validators.required]],
       phoneNumber: ['', [Validators.required, Validators.pattern('^\\+370[0-9]{8}')]],
       description: ['', [Validators.maxLength(500)]],
       // hastags pasidometi del validacijos
       tags: ['', [Validators.maxLength(500), Validators.pattern('(#[a-zA-Z]+,?)+[^,]$')]]
-    //  /#(\w*[0-9a-zA-Z]+\w*[0-9a-zA-Z])/g
+      //  /#(\w*[0-9a-zA-Z]+\w*[0-9a-zA-Z])/g
     });
   }
 
@@ -45,8 +45,8 @@ export class RegisterJobComponent implements OnInit {
     }
 
     if (this.registerJobForm.valid) {
-       this.router.navigate(['good-deeds']);
-       window.location.reload();
+      this.router.navigate(['good-deeds']);
+      window.location.reload();
 
     }
     this.addJob(this.registerJobForm.value);
@@ -68,7 +68,6 @@ export class RegisterJobComponent implements OnInit {
 
 
   }
-
 
 
 }
