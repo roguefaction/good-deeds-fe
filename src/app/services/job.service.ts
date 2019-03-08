47489bcb53d59d4a07001ec64aaa9eb1ad1e9816
Vytautas Kondratas <vytas.k@live.com>
2019-03-08 13:34:48 +0200

    Merge branch 'master' of https://github.com/roguefaction/good-deeds-fe into feature-edit-job
    # Conflicts:
    #	src/app/good-deeds-details/good-deeds-details.component.html
    #	src/app/register-job/register-job.component.html
    #	src/app/register-job/register-job.component.ts

--- src/app/services/job.service.ts
+++ src/app/services/job.service.ts
@@ -12,23 +12,24 @@ export class JobService {
   }
 
   getJobs(): Observable<Job[]> {
-    return this.http.get<Job[]>(`localhost:8080/job/`);
+    return this.http.get<Job[]>(`https://calm-waters-93672.herokuapp.com/job/`);
   }
 
   createAuthorizationHeader(headers: HttpHeaders) {
     headers.append('Content-Type', 'application/json');
   }
 
+
   addJob(job: Job) {
-    return this.http.post('localhost:8080/job/', job);
+    return this.http.post('https://calm-waters-93672.herokuapp.com/job/', job);
   }
 
   editJob(job: Job, id: number) {
-    return this.http.put('localhost:8080/job/' + id, job);
+    return this.http.put('https://calm-waters-93672.herokuapp.com/job/' + id, job);
   }
 
   getJobById(id: number): Observable<Job>  {
-    return this.http.get<Job>('localhost:8080/job/' + id);
+    return this.http.get<Job>('https://calm-waters-93672.herokuapp.com/job/' + id);
 
   }
 }
