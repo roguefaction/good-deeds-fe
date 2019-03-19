2b4eeb00b76ac098bed903b64a67af41315793f5
Rolanda Raudyte <rolanda763@gmail.com>
2019-03-19 14:59:31 +0200

    guards folder created user-profile component created

---
+++ src/app/models/user.ts
@@ -0,0 +1,13 @@
+export class User {
+  id: number;
+  email: string;
+  name: string;
+  password: string;
+  phone?: string;
+  token?: string;
+
+  constructor(email: string, password: string) {
+    this.email = email;
+    this.password = password;
+  }
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from '../models/user';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+  constructor(private http: HttpClient) { }
+
+  getAll() {
+    return this.http.get<User[]>('https://calm-waters-93672.herokuapp.com/users');
+  }
+}
